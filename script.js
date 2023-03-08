5b873ae678743340085534b3d25921f45e3d1f02
saikrishnareddy854 <saikrishnareddy123.gurijala@gmail.com>
2023-03-08 22:27:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const countDownDate = new Date("june 21,2023 00:00:00").getTime();
+const x = setInterval(function(){
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days    = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours   = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes   = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+    
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+
+    }
+
+},1000);
